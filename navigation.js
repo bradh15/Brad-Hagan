e0e7024fde67d5b01e40ba0534040980a211ffe4
bradh15 <haganbrad000@gmail.com>
2026-09-03 13:48:11 -0500

    Create navigation.js

---
+++ navigation.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("navigation").innerHTML = `
+        <nav class="navbar">
+            <a href="about.html">About Me</a>
+            <a href="research.html">Research</a>
+            <a href="teaching.html">Teaching</a>
+            <a href="personal.html">Personal</a>
+        </nav>
+    `;
+});
